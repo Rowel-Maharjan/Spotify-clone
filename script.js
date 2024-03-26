2640aea06061c9cc443e4ae295f878d29aad20b8
Rowel Maharjan <rowelmhj@gmail.com>
2024-03-26 14:28:24 +0545

    Next and previous added

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let currentSong = new Audio;
+let play = document.getElementById("playSong");
+let index
 
 function secondsToTime(seconds) {
     // Calculate minutes and remaining seconds
@@ -13,9 +15,10 @@ function secondsToTime(seconds) {
     return minutesString + ':' + secondsString;
 }
 
+//Get songs
 async function getSongs() {
-    // let a = await fetch("http://127.0.0.1:5500/songs/")
-    let a = await fetch("http://192.168.1.101:3000/songs/")
+    let a = await fetch("http://127.0.0.1:5500/songs/")
+    // let a = await fetch("http://192.168.1.101:3000/songs/")
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -27,19 +30,22 @@ async function getSongs() {
         if (Object.hasOwnProperty.call(as, key)) {
             const element = as[key];
             if (element.href.endsWith(".mp3") || element.href.endsWith(".m4a")) {
-                // songsName.push(element.title.slice(0, -4));
-                songsName.push(element.innerText.slice(0,-4))
+                songsName.push(element.title.slice(0, -4));
+                // songsName.push(element.innerText.slice(0,-4))
                 songs.push(element.href)
             }
         }
     }
-    return { songsName: songsName, songs: songs };
+    return { songsName: songsName, song: songs };
 }
 
+//To play songs that is in the track
 const playMusic = (track, name, pause = false) => {
     currentSong.src = track;
+
     if (!pause) {
         currentSong.play();
+        play.src = "/images/pause.svg"
     }
 
     let parts = name.split("-")
@@ -56,7 +62,7 @@ const playMusic = (track, name, pause = false) => {
 async function main() {
     // Get the list of all Songs 
     let songs = await getSongs();
-    playMusic(songs.songs[0], songs.songsName[0], true);
+    playMusic(songs.song[0], songs.songsName[0], true);
 
     // Show all songs in the playlist
     let SongUl = document.querySelector(".songList").getElementsByTagName("ul")[0];
@@ -72,6 +78,17 @@ async function main() {
         </div></li>`)
     }
     document.querySelector(".songList").getElementsByTagName("li")[0].style.backgroundColor = "black";
+    
+    //To change the color of playlist songs
+    function changecolor(change){
+        index = songs.song.indexOf(currentSong.src)
+        if (0 <= (index + change) && (index + change)< songs.song.length){
+            Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(item => {
+                item.style.backgroundColor = "";
+            })
+            document.querySelector(".songList").getElementsByTagName("li")[index+change].style.backgroundColor = "black";
+        }
+    }
 
     // Attach event listener to each songs 
     Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach((e, index) => {
@@ -80,13 +97,11 @@ async function main() {
                 item.style.backgroundColor = "";
             })
             e.style.backgroundColor = "black";
-            play.src = "/images/pause.svg";
-            playMusic(songs.songs[index], songs.songsName[index])
-        });
+            playMusic(songs.song[index], songs.songsName[index])
+        })
     });
 
     // Attach event listener to play, next and Previous
-    let play = document.getElementById("playSong");
     play.addEventListener("click", (element) => {
         if (currentSong.paused) {
             currentSong.play();
@@ -99,6 +114,7 @@ async function main() {
         }
 
     })
+
     // Listen for timeupdate event 
     currentSong.addEventListener("timeupdate", () => {
         document.querySelector(".passedTime").innerHTML = secondsToTime(currentSong.currentTime);
@@ -108,8 +124,16 @@ async function main() {
 
         document.querySelector(".color").style.width = (currentSong.currentTime / currentSong.duration) * 100 + "%";
 
-        if (document.querySelector(".circle").style.left == "99%")
-            play.src = "/images/playbutton.svg"
+        if(currentSong.currentTime == currentSong.duration){
+            changecolor(1);
+            if(index+1 < songs.song.length){
+                playMusic(songs.song[index + 1],songs.songsName[index+1]);
+            }
+            if(index+1 == songs.song.length){
+                currentSong.pause();
+                play.src = "/images/playbutton.svg"
+            }
+        }
     })
 
     // Event Listener to seekbar
@@ -136,13 +160,31 @@ async function main() {
     })
 
     //Hamburger effect
-    document.querySelector(".nav").addEventListener("click",()=>{
+    document.querySelector(".nav").addEventListener("click", () => {
         document.querySelector(".left").style.left = "1%";
-    })   
+    })
 
-    document.querySelector(".cancel").addEventListener("click",()=>{
+    document.querySelector(".cancel").addEventListener("click", () => {
         document.querySelector(".left").style.left = "-100%";
     })
+
+    //For next songs
+    next.addEventListener("click",()=>{
+        changecolor(1);
+        if(index+1 < songs.song.length){
+            play.src = "/images/pause.svg"
+            playMusic(songs.song[index + 1],songs.songsName[index+1]);
+        }
+    })
+
+    //For previous songs
+    previous.addEventListener("click",()=>{
+        changecolor(-1);
+        if(index-1 > -1){
+            playMusic(songs.song[index - 1],songs.songsName[index-1]);
+        }
+    })
+
 }
 
 main();
