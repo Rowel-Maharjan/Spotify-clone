161dd35e327bde90d233648ace7564aeebb859fc
Rowel Maharjan <rowelmhj@gmail.com>
2025-05-09 19:15:04 +0545

    Trying to host js

--- js/script2.js
+++ js/script2.js
@@ -122,7 +122,7 @@ function loadPlaylist() {
 }
 
 async function displayAlbums() {
-  let a = await fetch("http://127.0.0.1:5500/songs/");
+  let a = await fetch("songs/");
   // let a = await fetch(`http://192.168.1.101:3000/songs/`)
   let response = await a.text();
   let div = document.createElement("div");
